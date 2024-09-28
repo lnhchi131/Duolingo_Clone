2d46044c5644912bc81254a50dbaf24f39ede430
Chi <hoangthilinhchi131@gmail.com>
2024-09-28 23:08:56 +0700

    layout update

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'; // nếu bạn dùng Vue, hoặc React tùy vào framework của bạn
+import App from './App.vue'; // tương tự với component chính của bạn
+
+createApp(App).mount('#app');
